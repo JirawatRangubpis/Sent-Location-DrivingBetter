5ab4d712bf0684241df9f7b5cc035d03c4d1a71f
jirawat <sun27795_838@windowslive.com>
2016-02-25 16:24:31 +0700

    test

---
+++ app/src/main/java/swu/sun/yut/ob/sentlocationdrivingbetter/MainActivity.java
@@ -0,0 +1,13 @@
+package swu.sun.yut.ob.sentlocationdrivingbetter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
